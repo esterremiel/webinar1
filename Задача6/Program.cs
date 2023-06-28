e9edeff8ce76a988baad91f6247ce9eb16154062
Ester <ester.remiel@mail.ru>
2023-06-28 20:25:13 +0300

    задача6

---
+++ Задача6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int b = a%2;
+
+if(b == 1 || b == -1)
+{
+    Console.WriteLine("Нет");
+}
+else
+{
+    Console.WriteLine("Да");
+}
